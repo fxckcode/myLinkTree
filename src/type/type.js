7740a2ace6496bbb55f4e048be0741d7413ca97d
ArchDiego <diegoale8582@hotmail.com>
2023-02-12 14:57:10 -0500

    animation, typed.js and background

---
+++ src/type/type.js
@@ -0,0 +1,8 @@
+import Typed from 'typed.js';
+var typed = new Typed('.multiText', {
+    strings: ["FullStack Developer", "Web Developer", "UI/UX Designer", "Freelancer"],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1500
+});
